a36d6598619c817ff76be104837428eff0027ad9
crystaller347 <qwertymum04@gmail.com>
2024-12-02 15:15:56 +0200

    implement Redux Persist

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,9 @@
-import { useState, useEffect } from 'react';
 import css from './App.module.css';
-import contactsData from "../../contactsData.json";
 import ContactForm from "../ContactForm/ContactForm.jsx";
 import SearchBox from "../SearchBox/SearchBox.jsx";
 import ContactList from "../ContactList/ContactList.jsx";
 
 export default function App() {
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = window.localStorage.getItem("saved-contacts")
-    if (savedContacts !== null) {
-      return JSON.parse(savedContacts)
-    }
-    return contactsData;
-  })
-
-  useEffect(() => {
-    window.localStorage.setItem("saved-contacts", JSON.stringify(contacts))
-  }, [contacts])
-
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
